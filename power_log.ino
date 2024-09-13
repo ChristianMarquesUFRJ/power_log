e764e7aec37e3aca9343739a0429692b33871a84
ChristianMarquesUFRJ <christianmarquesos@poli.ufrj.br>
2024-09-13 16:57:36 -0300

    Botão de start log
    Adição do botão que inicializa a coleta ao ser pressionado e para a coleta após ser apertado novamente.

--- power_log.ino
+++ power_log.ino
@@ -5,9 +5,30 @@
 
 Adafruit_INA219 ina219;
 
+const int pin_on_off = 4;
+const int pin_led = 5;
+bool logger_on = false;
+bool first_time = true;
+
+void IRAM_ATTR toggle_on_off()
+{
+    if (first_time){
+        first_time = false;
+        return;
+    }
+    digitalWrite(pin_led, logger_on);
+    logger_on = !logger_on;
+}
+
 void setup(void) 
 {
     Serial.begin(115200);
+    
+    // Botao de on/off
+    pinMode(pin_on_off, INPUT);
+    pinMode(pin_led, OUTPUT);
+    attachInterrupt(pin_on_off, toggle_on_off, RISING);
+    digitalWrite(pin_led, HIGH);
 
     while (!Serial) {
         delay(1);
@@ -45,15 +66,20 @@ void setup(void)
 }
 
 void loop(void) {
+    static unsigned int start_time_ms = millis();
     float shuntvoltage = ina219.getShuntVoltage_mV();
     float busvoltage = ina219.getBusVoltage_V();
     float current_mA = ina219.getCurrent_mA();
     float power_mW = ina219.getPower_mW();
     float load_voltage = busvoltage + (shuntvoltage / 1000);
-    static unsigned int start_time_ms = millis();
 
-    String msg = get_message(millis()-start_time_ms, load_voltage, current_mA, power_mW);
-    appendFile(SD, "/datalogger.txt", msg);
-
+    if (logger_on){
+        String msg = get_message(millis()-start_time_ms, load_voltage, current_mA, power_mW);
+        appendFile(SD, "/datalogger.txt", msg);
+    }
+    else{
+        Serial.println(">> Sem coleta de dados! Apertar o botao para começar.");
+        //start_time_ms = millis(); Iria comecar do zero o contador de tempo do append, mas acho melho o contador continuar para vermos o gap sem coleta
+    }
     delay(1000);
 }
